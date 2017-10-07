3d6eaeef36f2c317ce4334611f9a115cd1969771
Giulio <giulio.m.frasca@gmail.com>
2017-10-07 13:08:24 -0400

    BUGFIX: Copy response list instead of referencing

--- psgroupme/main.py
+++ psgroupme/main.py
@@ -1,9 +1,11 @@
 from rest import main as start_rest_bots
 from timed import main as start_timed_bots
 
+
 def main():
     start_rest_bots()
     start_timed_bots()
+
 
 if __name__ == '__main__':
     main()
--- psgroupme/rest.py
+++ psgroupme/rest.py
@@ -5,13 +5,18 @@ import bots
 
 def main():
     """Start up a Flask REST API Frontent"""
+    local = False
+
     # Setup the Flask App
     app = Flask(__name__)
     api = Api(app)
 
     # Add Bots to a REST API URL here
-    api.add_resource(bots.ScheduleBot, '/schedulebot')
-    api.add_resource(bots.HockeyBot, '/hockeybot')
+    if local:
+        api.add_resource(bots.TestBot, '/testbot')
+    else:
+        api.add_resource(bots.ScheduleBot, '/schedulebot')
+        api.add_resource(bots.HockeyBot, '/hockeybot')
 
     # Start up the REST API
     app.run(host='0.0.0.0', port='5002')
