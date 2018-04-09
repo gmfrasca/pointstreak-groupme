85b485a8f3396dade4f97248abcf1a07b483496b
Giulio <giulio.m.frasca@gmail.com>
2018-04-09 14:52:58 -0400

    Normalize Incoming Gametimes

--- psgroupme/parsers/schedules/game.py
+++ psgroupme/parsers/schedules/game.py
@@ -1,9 +1,13 @@
 import parsedatetime as pdt
 import datetime
 
+DATE_DESCRIPTOR = "%a, %b %d"
+# TIME_DESCRIPTOR = "%H:%M"  # 24-hour
+TIME_DESCRIPTOR = "%I:%M %p"  # 12-hour
+FULL_DESCRIPTOR = "{0} {1}".format(DATE_DESCRIPTOR, TIME_DESCRIPTOR)
 
 # TIME_DESCRIPTOR = "%a %b %d %H:%M"  # 24-hour
-TIME_DESCRIPTOR = "%a %b %d %I:%M%p"  # 12-hour
+# TIME_DESCRIPTOR = "%a %b %d %I:%M%p"  # 12-hour
 
 
 class Game(object):
@@ -12,14 +16,34 @@ class Game(object):
     def __init__(self, date, time, hometeam, homescore, awayteam, awayscore,
                  year=None):
         """ Store this game's relevant data """
-        self.date = date.strip()
-        self.time = time.strip()
+        self.date = self.normalize_date(date.strip())
+        self.time = self.normalize_time(time.strip())
         self.full_gametime = self.assemble_full_gametime(date, time, year)
-        self.full_gametime_str = self.full_gametime.strftime(TIME_DESCRIPTOR)
+        self.full_gametime_str = self.full_gametime.strftime(FULL_DESCRIPTOR)
         self.hometeam = hometeam
         self.homescore = homescore
         self.awayteam = awayteam
         self.awayscore = awayscore
+
+    def determine_year(self, date, year=None):
+        now = datetime.datetime.now()
+        year = str(year if year else now.year)
+        full_gametime = date.split()[1:]
+        if full_gametime[0] in ['Jan', 'Feb', 'Mar'] and now.month > 7:
+            year = str(now.year + 1)
+        return year
+
+    def normalize_date(self, date, year=None):
+        year = self.determine_year(date, year)
+        gamedate = date.split()[1:]
+        gamedate.extend([year])
+        gamedate = '{} {} {}'.format(*gamedate)
+        parsed, status = pdt.Calendar().parseDT(str(gamedate))
+        return parsed.strftime(DATE_DESCRIPTOR)
+
+    def normalize_time(self, time):
+        parsed, status = pdt.Calendar().parseDT(str(time))
+        return parsed.strftime(TIME_DESCRIPTOR)
 
     def assemble_full_gametime(self, date, time, year=None):
         """
@@ -33,13 +57,8 @@ class Game(object):
         Returns:
             a parsed pdt object representing the input date and time
         """
-        now = datetime.datetime.now()
-        # TODO: right now we assume games are in the same year because
-        # Pointstreak does not give us a better way to determine it
-        year = str(year if year else now.year)
+        year = self.determine_year(date, year)
         full_gametime = date.split()[1:]
-        if full_gametime[0] in ['Jan', 'Feb', 'Mar'] and now.month > 7:
-            year = str(now.year + 1)
         full_gametime.extend([year])
         full_gametime.extend(time.split())
         full_gametime = ' '.join(full_gametime)
