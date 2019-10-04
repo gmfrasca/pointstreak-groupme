68a1c98db20726403bf3cd0e4f504fbd140a50be
gfrasca <gfrasca@redhat.com>
2019-10-04 15:14:55 -0400

    Add Dash Platform TeamStats Parser

--- psgroupme/factories/team_stats_factory.py
+++ psgroupme/factories/team_stats_factory.py
@@ -1,5 +1,7 @@
 from psgroupme.parsers.team_stats.sportsengine_team_stats import (
     SportsEngineTeamStats)
+from psgroupme.parsers.team_stats.dashplatform_team_stats import (
+    DashPlatformTeamStats)
 
 
 class TeamStatsFactory(object):
@@ -7,6 +9,8 @@ class TeamStatsFactory(object):
     def create(stats_type, **kwargs):
         if stats_type == 'sportsengine':
             return SportsEngineTeamStats(**kwargs)
+        elif stats_type == 'dash':
+            return DashPlatformTeamStats(**kwargs)
         else:
             raise ValueError("Stats Tool Type '{0}' not found"
                              .format(stats_type))
