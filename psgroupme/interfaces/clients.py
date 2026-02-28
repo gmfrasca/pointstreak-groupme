6cbd53be4f5ef371d56c21a49099d3af7657cef4
Giulio Frasca <giulio.m.frasca@gmail.com>
2026-02-27 21:20:40 -0500

    chore: Encap rest clients into a Manager class
    Signed-off-by: Giulio Frasca <giulio.m.frasca@gmail.com>

---
+++ psgroupme/interfaces/clients.py
@@ -0,0 +1,82 @@
+from flask import Flask
+from flask_restful import Api
+from psgroupme.config_manager import ConfigManager
+from psgroupme.interfaces.listener import GroupmeListener
+from functools import reduce
+import sys
+import logging
+import psgroupme.bots as bots # noqa: need this to subclass
+
+
+class ClientManager(object):
+    def __init__(self, config_path):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self.cm = ConfigManager(config_path)
+        self.fcm = FlaskClientManager(config_path)
+        self.create_bots()
+
+    def str_to_class(self, class_name):
+        return reduce(getattr, class_name.split("."), sys.modules[__name__])
+
+    def create_bots(self):
+        for bot_cfg in self.cm.get_bots():
+            class_name = bot_cfg.get('class_name')
+            bot_class = self.str_to_class(class_name)
+
+            # Handle Legacy configuration options
+            bot_url = bot_cfg.get('bot_url')
+            default_listener_cfg = {"type": "groupme", "url": bot_url}
+
+            # Create bot
+            bot = bot_class(bot_cfg=bot_cfg)
+
+            # Set up listeners
+            listeners_cfg = bot_cfg.pop('listeners', [default_listener_cfg])
+            for l in listeners_cfg:
+                self.add_listener(bot, l, bot_url)
+    
+    def add_listener(self, bot, listener_cfg, bot_url):
+        listener_type = listener_cfg.get('type', 'groupme')
+        if listener_type == 'groupme':
+            bot_url = listener_cfg.get('url', bot_url)
+            if bot_url is not None:
+                self._logger.info("Adding Groupme listener for bot {} at {}".format(bot.bot_name, bot_url))
+                self.fcm.add_bot(GroupmeListener, bot_url, {'bot': bot})
+            else:
+                self._logger.warning("Bot URL is not set, cannot add Groupme listener")
+        else:
+            self._logger.warning("Unknown listener type '{0}', bot will not have a listener".format(listener_type))
+
+    def run(self):
+        self.fcm.run()
+
+
+class FlaskClientManager(object):
+    def __init__(self, config_path):
+        self.cm = ConfigManager(config_path)
+        self.img_cfg = self.cm.get_img_server_config()
+        self.host = '0.0.0.0'
+        self.port = self.cm.get_flask_port()
+
+        self.app = Flask(__name__,
+                         static_folder=self.img_cfg.get('path'),
+                         static_url_path=self.img_cfg.get('dest'))
+        self.api = Api(self.app)
+
+    def add_bot(self, bot_class, bot_url, bot_kwargs):
+        self.api.add_resource(bot_class, bot_url, resource_class_kwargs=bot_kwargs)
+
+    def run(self):
+        self.app.run(host=self.host, port=self.port)
+
+
+
+
+def main():
+    """Start up a Client Manager"""
+    client_manager = ClientManager(config_path=None)
+    client_manager.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_clients.py
@@ -0,0 +1,38 @@
+from psgroupme.interfaces.clients import main as clients_main
+import unittest
+import mock
+
+MOCK_CFG = {
+    'bots': [
+        {
+            'class_name': 'bots.BaseBot',
+            'bot_name': 'BaseBot',
+            'bot_id': '0',
+            'bot_url': '/basebot',
+            'group_name': 'foo',
+            'group_id': '12345',
+            'callback_url': 'http://foo.bar',
+            'avatar_url': 'http://funny.jpg'
+        }
+    ],
+    'img': {
+        'dest': '/img/'
+    }
+}
+
+
+class TestClients(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @mock.patch('psgroupme.interfaces.rest.Flask.run')
+    @mock.patch('psgroupme.interfaces.rest.ConfigManager')
+    def test_app_runs(self, mock_cm, mock_flask_runner):
+        mock_cm.get_bots.return_value = MOCK_CFG['bots']
+        mock_cm.get_img_server_config = MOCK_CFG['img']
+        clients_main()
+        mock_flask_runner.assert_called_once()
